e5fb24b8f8a0d5c68faefecf53ac99f40cd33f19
arcilli <raileanu.gabriel97@gmail.com>
2021-04-15 10:12:16 +0300

    2. Values, variables & types

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,24 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  // vals are immutable
+  val x = 42
+  println(x)
+
+  val aString: String = "hello"
+
+  println(aString)
+
+  val aShort: Short = 4613
+
+  val aLong: Long = 52739851532L
+
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // mutable
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+}
