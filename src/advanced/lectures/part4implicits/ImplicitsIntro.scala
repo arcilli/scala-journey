6f67b02a98f58f876695f7ca93a1e9aa5fe56794
arcilli <raileanu.gabriel97@gmail.com>
2021-09-14 14:43:07 +0300

    33. Enter implicits

--- src/advanced/lectures/part4implicits/ImplicitsIntro.scala
+++ src/advanced/lectures/part4implicits/ImplicitsIntro.scala
@@ -1,5 +1,29 @@
 package advanced.lectures.part4implicits
 
-object ImplicitsIntro {
+object ImplicitsIntro extends App {
+  val pair = "Daniel" -> "555"
+  val intPar = 1 -> 2
+
+  case class Person(name: String) {
+    def greet = s"Hi, my name is $name"
+  }
+
+  implicit def fromStringToPerson(str: String): Person = Person(str)
+
+  println("Peter".greet) // println(fromStrinToPerson("Peter").greet)
+
+//  class A {
+//    def greet: Int = 2
+//  }
+//
+//  implicit def fromStringToA(str: String): A = new A // This will not work since the compiler is confused on which implicit to use
+
+  // implicit parameters
+  def increment(x: Int)(implicit amount: Int): Unit = x + amount
+
+  implicit val defaultAmount = 10
+
+  increment(2)
+  // NOT default args
 
 }
